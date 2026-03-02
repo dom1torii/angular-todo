6427708131bea9a27beec38497b8632ed082169f
dom1torii <contact@domitori.xyz>
2026-03-02 05:17:00 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,72 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { form, FormField } from '@angular/forms/signals';
+
+import { Task } from '../components/task';
+
+export interface TaskData {
+  id: number;
+  task: string;
+  important: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormField, Task],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
+
 export class App {
-  protected readonly title = signal('angular-test');
+  taskModel = signal<TaskData>({
+    id: 0,
+    task: '',
+    important: false,
+  })
+
+  taskForm = form(this.taskModel);
+
+  tasks = signal<TaskData[]>([])
+  completed = signal<TaskData[]>([])
+
+  private nextTaskId = 1;
+
+  resetForm() {
+    this.taskForm.task().value.set("")
+    this.taskForm.important().value.set(false)
+  }
+
+  addTask() {
+    if (this.taskForm.task().value() === "") return;
+    const newTask: TaskData = {
+      id: this.nextTaskId++,
+      task: this.taskForm.task().value(),
+      important: this.taskForm.important().value(),
+    };
+
+    console.log(newTask)
+
+    if (newTask.task.trim()) {
+      this.tasks.update(tasks => [...tasks, newTask])
+      console.log(this.tasks())
+      this.resetForm()
+    }
+  }
+
+  deleteTask(id: number) {
+    this.tasks.update(tasks => tasks.filter(task => task.id !== id));
+  }
+
+  completeTask(id: number) {
+    const toComplete = this.tasks().find(task => task.id === id)
+
+    if (!toComplete) return;
+
+    this.completed.update(completed => [...completed, toComplete]);
+    this.deleteTask(id)
+  }
+
+  deleteCompleted(id: number) {
+    this.completed.update(completed => completed.filter(task => task.id !== id))
+  }
 }
---
+++ src/components/task.ts
@@ -0,0 +1,108 @@
+import { Component, input, output } from '@angular/core';
+import { TaskData } from '../app/app'
+
+@Component({
+  selector: 'task',
+  template: `
+    <div class="task">
+      @if (task().important) {
+        <div class="task-important">!</div>
+      }
+      <div class="task-content">
+        <p class="task-text">
+          {{ task().task }}
+        </p>
+        <div class="task-actions">
+          <button class="task-delete" (click)="onDelete()">X</button>
+          @if (!completed()) {
+            <button class="task-complete" (click)="onComplete()">✔</button>
+          }
+        </div>
+      </div>
+
+    </div>
+  `,
+  styles: `
+    .task {
+      display: flex;
+      align-items: stretch;
+      border-radius: 5px;
+      border: 1px solid rgba(255, 255, 255, 0.3);
+      background-color: rgba(255, 255, 255, 0.05);
+      min-height: 50px;
+      overflow: hidden;
+    }
+
+    .task-important {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: orangered;
+      width: 20px;
+      font-size: 30px;
+    }
+
+    .task-content {
+      display: flex;
+      height: 100%;
+      flex-direction: row;
+      align-items: center;
+      padding: 10px;
+      flex: 1;
+    }
+
+    .task-text {
+      margin: 0;
+      white-space: normal;
+      word-break: break-word;
+      overflow-wrap: break-word;
+    }
+
+    .task-actions {
+      display: flex;
+      flex-direction: row;
+      margin-left: auto;
+      gap: 3px;
+    }
+
+    button {
+      background: none;
+      border: none;
+      border-radius: 5px;
+      font-size: 20px;
+      color: white;
+      width: 30px;
+      aspect-ratio: 1;
+      cursor: pointer;
+      transition: all 0.1s;
+    }
+
+    button.task-delete {
+      background-color: indianred;
+    }
+
+    button.task-complete {
+      background-color: seagreen;
+    }
+
+    button:hover {
+      filter: grayscale(50%)
+    }
+  `
+})
+
+export class Task {
+  task = input.required<TaskData>();
+  completed = input(false);
+
+  delete = output<number>();
+  complete = output<number>();
+
+  onDelete() {
+    this.delete.emit(this.task().id);
+  }
+
+  onComplete() {
+    this.complete.emit(this.task().id);
+  }
+}
